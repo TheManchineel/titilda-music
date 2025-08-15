e192819c216d6a05b8342ae453e9e82728e29b9e
Emanuel Mihali <81972898+Archibugiere@users.noreply.github.com>
2025-08-15 23:17:44 +0300

    feat: use hikaricp

--- src/main/java/org/titilda/music/base/database/DatabaseManager.java
+++ src/main/java/org/titilda/music/base/database/DatabaseManager.java
@@ -1,64 +1,36 @@
 package org.titilda.music.base.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.titilda.music.base.config.ConfigManager;
 
 import java.sql.*;
 
 public class DatabaseManager {
 
-    private static Connection c = null;
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl(ConfigManager.getString(ConfigKey.DATABASE_URL));
+        config.setUsername(ConfigManager.getString(ConfigKey.DATABASE_USER));
+        config.setPassword(ConfigManager.getString(ConfigKey.DATABASE_PASSWORD));
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
+    private DatabaseManager() {}
 
     /**
-     * Static block to load the PostgreSQL JDBC driver.
-     * This is necessary to ensure the driver is registered before any connection attempts.
-     */
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("PostgreSQL JDBC Driver not found. Include it in your library path.", e);
-        }
-    }
-    /**
-     * Returns a connection to the PostgreSQL database.
+     * Gets a connection to the postgres database using HikariCP.
+     *
+     * @return a Connection object
+     * @throws SQLException if a database access error occurs
      */
     public static Connection getConnection() throws SQLException {
-
-        try {
-            c = DriverManager.getConnection(
-                    ConfigManager.getString(ConfigManager.ConfigKey.DATABASE_URL),
-                    ConfigManager.getString(ConfigManager.ConfigKey.DATABASE_USER),
-                    ConfigManager.getString(ConfigManager.ConfigKey.DATABASE_PASSWORD)
-            );
-        } catch (Exception e) {
-            System.err.println("Error connecting to the database: " + e.getMessage());
-            throw new SQLException("Failed to connect to the database", e);
-        }
-        System.out.println("Opened database successfully");
-        return c;
-    }
-
-    public String getPasswordHash(String username) {
-        String passwordHash = null;
-        String sql = "SELECT password_hash FROM Users WHERE username = ?";
-        
-        try (Connection conn = getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            
-            pstmt.setString(1, username);
-            
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    passwordHash = rs.getString("password_hash");
-                }
-            }
-            
-        } catch (SQLException e) {
-            System.err.println("Error retrieving password hash for user " + username + ": " + e.getMessage());
-            throw new RuntimeException("Failed to retrieve password hash", e);
-        }
-        
-        return passwordHash;
+        return ds.getConnection();
     }
 
 }
