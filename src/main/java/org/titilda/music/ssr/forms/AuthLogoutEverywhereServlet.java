7e11cab6fbc889e32c5d9301d255a547c7feaa16
A.M <37479927+TheManchineel@users.noreply.github.com>
2025-09-03 22:54:25 +0200

    fix(ssr): make auth logout everywhere work on SSR

--- src/main/java/org/titilda/music/ssr/forms/AuthLogoutEverywhereServlet.java
+++ src/main/java/org/titilda/music/ssr/forms/AuthLogoutEverywhereServlet.java
@@ -4,30 +4,30 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.titilda.music.base.controller.Authentication;
 import org.titilda.music.base.database.DAO;
 import org.titilda.music.base.database.DatabaseManager;
+import org.titilda.music.base.model.User;
+import org.titilda.music.ssr.BaseAuthenticatedPostWithRedirectServlet;
 import org.titilda.music.ssr.BasePostWithRedirectServlet;
+import org.titilda.music.ssr.CookieTokenInterface;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
 
 @WebServlet(urlPatterns = { "/auth/logout-everywhere" })
-public final class AuthLogoutEverywhereServlet extends BasePostWithRedirectServlet {
+public final class AuthLogoutEverywhereServlet extends BaseAuthenticatedPostWithRedirectServlet implements CookieTokenInterface {
     @Override
-    protected String processRequestAndRedirect(HttpServletRequest request, HttpServletResponse response) {
-        try (Connection connection = DatabaseManager.getConnection()) {
-            DAO dao = new DAO(connection);
-            Date now = new Date();
-            now = new Date(now.getTime() / 1000L * 1000L);
-            dao.setInvalidationDate(request.getParameter("username"), now);
-            Cookie cookie = generateTokenCookie("");
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
-            return "/login";
-        }
-        catch (SQLException _) {
+    protected String processRequestAndRedirect(HttpServletRequest request, HttpServletResponse response, User user) {
+        try {
+            Authentication.invalidateAllSessions(user);
+        } catch (Authentication.FailedToInvalidateSessionException _) {
             return "/error?error=internal_server_error";
         }
+        Cookie cookie = generateTokenCookie("");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "/login";
     }
 }
